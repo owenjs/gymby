0b785b25a317840b7def77c7a19dfeb0e23bdd08
Owen Evans <owen@3sidedcube.com>
2021-10-28 16:25:32 +0100

    startup name change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import "dotenv/config.js";
 import logger from "src/logger";
 import express from "express";
-import config from "src/startup";
+import startup from "src/startup";
 
 const app = express();
 
-config(app);
+startup(app);
 
 const server = app.listen(process.env.PORT || 3000, () => {
   const { address, port } = server.address();
