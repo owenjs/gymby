d20f9185873a7c823c64687f948604966f6b73c8
Owen Evans <owen@3sidedcube.com>
2021-11-04 11:41:18 +0000

    absolute paths added for ui app

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/logo.svg";
+import logo from "/@/assets/logo.svg";
 
 function App() {
   const [count, setCount] = useState(0);
--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./css/index.scss";
-import App from "./components/App";
+import "/@/css/index.scss";
+import App from "/@/components/App";
 
 ReactDOM.render(
   <React.StrictMode>
