9b48f02fb100a2c57569af269a607efdc0491d3f
Owen Evans <owen@3sidedcube.com>
2021-11-19 11:35:28 +0000

    lint-stage test 4 revert

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -4,7 +4,6 @@ import validate from "src/middleware/validate";
 import bcrypt from "bcrypt";
 const router = Router();
 
-// Test
 router.post("/", validate(validateUser), async (req, res) => {
   let token = req.header("x-auth-token");
   if (token) return res.status(400).send("User already logged in");
