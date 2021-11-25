956b8e00bc2a5b2bc4255adb20c765cac45f3bf7
Owen Evans <owen@3sidedcube.com>
2021-11-25 12:47:17 +0000

    added react query and axios

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import queryClient from "/@/api/client";
 import configureStore from "/@/configureStore";
 import { BrowserRouter as Router } from "react-router-dom";
 import Pages from "/@/pages";
@@ -11,9 +14,13 @@ const store = configureStore();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <Pages />
-      </Router>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <Pages />
+
+          <ReactQueryDevtools initialIsOpen={false} />
+        </Router>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
